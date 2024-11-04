2a8672e2524071d585d74aafb5ccd8ab39bf1d27
Igorkeske <134227440+igorkeske@users.noreply.github.com>
2024-11-04 16:05:39 -0300

    Projeto de Eng. de Software
    link do notion: https://igorkeske.notion.site/Trabalho-engenharia-de-software-1268720391bb802583b3f5b7c13b9c4e

---
+++ Projeto eng. de software/src/shows.py
@@ -0,0 +1,17 @@
+# src/shows.py
+
+class Show:
+    def __init__(self, banda, data, local):
+        self.banda = banda
+        self.data = data
+        self.local = local
+
+class ListaDeShows:
+    def __init__(self):
+        self.shows = []
+
+    def adicionar_show(self, show):
+        self.shows.append(show)
+
+    def listar_shows(self):
+        return [(show.banda, show.data, show.local) for show in self.shows]
